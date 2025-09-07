7dca61a3503fbd061027c082b2a4fd792448bf83
.DEVitor-Lopes <vitorlopesds@gmail.com>
2025-09-07 01:33:12 +0000

    RESULTADO FINAL DESAFIO NOVATO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,9 +8,11 @@
 
 int main() {
 
+    // DECLARAÇAO DAS VARIAVEIS CARTA 1 E CARTA 2
+
     //VARIAVEIS CARTA 1
     char letrac1;
-    char cidadec1[50];
+    char cidadec1[50]; //RECEBE 49 CARACTERES
     int codigoc1;
     int populacaoc1;
     int turisticoc1;
@@ -18,9 +20,8 @@ int main() {
     float pibc1;
     
     // VARIAVEIS CARTA 2
-
-    char letrac2;
-    char cidadec2[50];
+    char letracard2;
+    char cidadec2[50]; // RECEBE 49 CARACTERES
     int codigoc2;
     int populacaoc2;
     int turisticoc2;
@@ -36,35 +37,31 @@ int main() {
     printf("---------------------------------- \n");
 
         //RECEBENDO OS DADOS - CARTA 1
-        printf("Digite uma letra entre 'A'  e 'H' que representará o estado:");
+        printf("Digite uma letra entre 'A'  e 'H' que representará o estado: ");
         scanf("%c", &letrac1);
 
-        printf("Digite o número da carta: \n");
+        printf("Digite o número da carta: ");
         scanf("%d", &codigoc1);
 
         // Limpa o buffer antes do fgets
         while ((getchar()) != '\n'); // descarta '\n' o 'ENTER'
 
-        printf("Digite o nome da cidade: \n");
-        fgets(cidadec1, 50, stdin); 
+        printf("Digite o nome da cidade: ");
+        fgets(cidadec1, 50, stdin); //fgets usado para leitura de strings com espaços.
 
-        printf("Digite a população da cidade: \n");
+        printf("Digite a população da cidade: ");
         scanf("%d", &populacaoc1);
 
-        printf("Digite a Área em KM² da cidade: \n");
+        printf("Digite a Área em KM² da cidade: ");
         scanf("%f", &areac1);
 
-        printf("Digite o PIB da cidade: \n");
+        printf("Digite o PIB da cidade: ");
         scanf("%f", &pibc1);
 
-        printf("Digite número de pontos turísticos da cidade:\n");
+        printf("Digite número de pontos turísticos da cidade: ");
         scanf("%d", &turisticoc1);
+        
 
-         // Limpa o buffer antes do fgets
-        while ((getchar()) != '\n'); // descarta '\n' o 'ENTER'
-
-
-        system("clear"); // LIMPA A TELA
     //=========================================================================================
 
     //RECEBENDO OS DADOS - CARTA 2
@@ -72,8 +69,11 @@ int main() {
     printf("    Insira os dados da carta 2: \n");
     printf("---------------------------------- \n");
 
+         // Limpa o buffer antes do fgets
+        while ((getchar()) != '\n'); // descarta '\n' o 'ENTER'
+
         printf("Digite uma letra entre 'A'  e 'H' que representará o estado: ");
-        scanf("%c", &letrac2);
+        scanf("%c", &letracard2);
 
         printf("Digite o número da carta: ");
         scanf("%d", &codigoc2);
@@ -100,30 +100,30 @@ int main() {
 
 //===================================================================================
 
-        //EXIBIÇÃO DAS CARTAS APÓS A LEITURA DE DADOS
-            //CARTA NUMERO 1
-            printf("---------------------------------- \n");
-            printf("            CARTA N°1: \n");
-            printf("---------------------------------- \n");
-                printf("ESTADO: %c \n", letrac1);
-                printf("CÓDIGO: %c%d \n", letrac1, codigoc1); //EXIBE A LETRA QUE CORRESPONDE AO ESTADO E O NUMERO DA CARTA, AMBOS OS DADOS FORMAM O CÓDIGO DA CARTA.
-                printf("NOME DA CIDADE: %s", cidadec1);
-                printf("POPULAÇÃO: %d \n", populacaoc1);
-                printf("ÁREA: %.2f KM² \n", areac1);
-                printf("PIB: %.2f bilhões de reais \n", pibc1);
-                printf("NÚMERO DE PONTOS TURISTICOS: %d \n", turisticoc1);
-        //-----------------------------------------------------------------
-            //CARTA NUMERO 2
-            printf("---------------------------------- \n");
-            printf("            CARTA N°2: \n");
-            printf("---------------------------------- \n");
-                printf("ESTADO: %c \n", letrac2);
-                printf("CÓDIGO: %c%d \n", letrac2, codigoc2); //EXIBE A LETRA QUE CORRESPONDE AO ESTADO E O NUMERO DA CARTA, AMBOS FORMAM O CÓDIGO DA CARTA.
-                printf("NOME DA CIDADE: %s", cidadec2);
-                printf("POPULAÇÃO: %d \n", populacaoc2);
-                printf("ÁREA: %.2f KM² \n", areac2);
-                printf("PIB: %.2f bilhões de reais \n", pibc2);
-                printf("NÚMERO DE PONTOS TURISTICOS: %d \n", turisticoc2);
+    //EXIBIÇÃO DAS CARTAS APÓS A LEITURA DE DADOS
+    //CARTA NUMERO 1
+    printf("---------------------------------- \n");
+    printf("            CARTA N°1: \n");
+    printf("---------------------------------- \n");
+        printf("ESTADO: %c \n", letrac1);
+        printf("CÓDIGO: %c%d \n", letrac1, codigoc1); //EXIBE A LETRA QUE CORRESPONDE AO ESTADO E O NUMERO DA CARTA, AMBOS OS DADOS FORMAM O CÓDIGO DA CARTA.
+        printf("NOME DA CIDADE: %s", cidadec1);
+        printf("POPULAÇÃO: %d \n", populacaoc1);
+        printf("ÁREA: %.2f KM² \n", areac1);
+        printf("PIB: %.2f bilhões de reais \n", pibc1);
+        printf("NÚMERO DE PONTOS TURISTICOS: %d \n", turisticoc1);
+    //-----------------------------------------------------------------
+    //CARTA NUMERO 2
+    printf("---------------------------------- \n");
+    printf("            CARTA N°2: \n");
+    printf("---------------------------------- \n");
+        printf("ESTADO: %c \n", letracard2);
+        printf("CÓDIGO: %c%d \n", letracard2, codigoc2); //EXIBE A LETRA QUE CORRESPONDE AO ESTADO E O NUMERO DA CARTA, AMBOS FORMAM O CÓDIGO DA CARTA.
+        printf("NOME DA CIDADE: %s", cidadec2);
+        printf("POPULAÇÃO: %d \n", populacaoc2);
+        printf("ÁREA: %.2f KM² \n", areac2);
+        printf("PIB: %.2f bilhões de reais \n", pibc2);
+        printf("NÚMERO DE PONTOS TURISTICOS: %d \n", turisticoc2);
 
 
     return 0;
